de9f339c539576e3f5d87b5fce79fd9f07e046fa
pradiph31 <pradip.hamal31@gmail.com>
2021-06-20 22:06:24 -0700

    Fetch courses from api
    After running the api server in port 44377, dyanmically generate the list of courses in react front end
    The list is not filtered for the student

--- WebApplication1/WebApplication1/clientapp/src/components/student/StudentCourse.js
+++ WebApplication1/WebApplication1/clientapp/src/components/student/StudentCourse.js
@@ -1,85 +1,119 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-}));
-
 const StudentCourse = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("/Classes")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }));
+
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+  let itemList;
+  const flexContainer = {
+    display: 'flex',
+    flexDirection: 'row',
 
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Paleontology 101"
-        subheader="June 14, 2021"
-      />
-      <CardMedia
-        className={classes.media}
-        image="/static/images/paleontology-101.jpg"
-        title="Palentology 101"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-        Paleontology is the scientific study of life that existed prior to, 
-        and sometimes including, the start of the Holocene Epoch (roughly 11,700 years before present)
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-      </Collapse>
-    </Card>
-  );
+  };
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    itemList = items.map(item => {
+      return (
+        <div>
+          <Card className={classes.root}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  {item.className.split('')[0]}
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={item.className}
+              subheader="June 14, 2021"
+            />
+            <CardMedia
+              className={classes.media}
+              image="/static/images/paleontology-101.jpg"
+              title="Palentology 101"
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {item.classDescription}
+              </Typography>
+            </CardContent>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+            </Collapse>
+          </Card>
+        </div>
+      )
+    })
+    return (
+      <div>
+        {itemList}
+      </div>
+    );
+  }
 }
 
-export default StudentCourse;
+export default StudentCourse
