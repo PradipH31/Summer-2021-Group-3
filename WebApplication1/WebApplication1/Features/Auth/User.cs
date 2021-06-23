23f31722081f2ea08b96eecaf968789ed40dbd0e
Jonathon Barber <Jonathon2997@gmail.com>
2021-06-23 16:10:38 -0500

    Added Authentication

---
+++ WebApplication1/WebApplication1/Features/Auth/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Features.Auth
+{
+    public class User
+    {
+    }
+}
--- WebApplication1/WebApplication1/Features/Auth/UserRole.cs
+++ WebApplication1/WebApplication1/Features/Auth/UserRole.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1.Features.Users
+namespace WebApplication1.Features.Auth
 {
-    public class Users
+    public class UserRole
     {
     }
 }
