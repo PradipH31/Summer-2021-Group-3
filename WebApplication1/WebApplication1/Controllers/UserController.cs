a5a66e3045b9eebafc62716c25fd564b71227204
Jonathon Barber <Jonathon2997@gmail.com>
2021-06-28 08:32:05 -0500

    Added DTOs; Added new Role(s)

---
+++ WebApplication1/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.Features.Auth;
+
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext context;
+        
+    }
+}
--- WebApplication1/WebApplication1/Features/Auth/Roles.cs
+++ WebApplication1/WebApplication1/Features/Auth/Roles.cs
@@ -8,6 +8,7 @@ namespace WebApplication1.Features.Auth
     internal static class Roles
     {
         public const string Admin = nameof(Admin);
+        public const string ClassAdmin = nameof(ClassAdmin);
         public const string Student = nameof(Student);
         public const string Instructor = nameof(Instructor);
         public const string Guest = nameof(Guest);
