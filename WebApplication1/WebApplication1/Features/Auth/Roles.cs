dbbb75074f265fe5c4e6243e9e88e45bbe4a8791
Jonathon Barber <Jonathon2997@gmail.com>
2021-06-27 20:34:28 -0500

    Added Admin role

--- WebApplication1/WebApplication1/Features/Auth/Roles.cs
+++ WebApplication1/WebApplication1/Features/Auth/Roles.cs
@@ -7,6 +7,7 @@ namespace WebApplication1.Features.Auth
 {
     internal static class Roles
     {
+        public const string Admin = nameof(Admin);
         public const string Student = nameof(Student);
         public const string Instructor = nameof(Instructor);
         public const string Guest = nameof(Guest);
