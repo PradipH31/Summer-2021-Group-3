46e00b45d935d0bad5ce80cfba26c2fc8659ce57
Caleb Charpentier <caleb.charpentier@selu.edu>
2021-06-28 21:21:11 -0500

    Fix migrations
    Fix migrations after adding second table

--- WebApplication1/WebApplication1/Migrations/20210629021346_Init.cs
+++ WebApplication1/WebApplication1/Migrations/20210629021346_Init.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace WebApplication1.Migrations
 {
-    public partial class AddClass : Migration
+    public partial class Init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -20,12 +21,30 @@ namespace WebApplication1.Migrations
                 {
                     table.PrimaryKey("PK_ClassDescription", x => x.Id);
                 });
+
+            migrationBuilder.CreateTable(
+                name: "ClassImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassImages", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "ClassDescription");
+
+            migrationBuilder.DropTable(
+                name: "ClassImages");
         }
     }
 }
