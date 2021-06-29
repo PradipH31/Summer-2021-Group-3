7ce7491b536933405ebc5236826035b28855f567
Caleb Charpentier <caleb.charpentier@selu.edu>
2021-06-28 21:20:26 -0500

    Class Image Model
    Add model for class images in database (proof of concept)

---
+++ WebApplication1/WebApplication1/Features/Classes/ClassImage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Features.Classes
+{
+    public class ClassImage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ContentType { get; set; }
+
+        public byte[] Data { get; set; }
+    }
+}
