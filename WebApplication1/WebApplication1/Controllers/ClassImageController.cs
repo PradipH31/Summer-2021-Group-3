81d32229d4486360b7f4cdf6222a8ea7214fc597
Caleb Charpentier <caleb.charpentier@selu.edu>
2021-06-28 22:00:17 -0500

    PostClassImage
    Controller to add files to database. Currently tested with .txt files and .pngs

---
+++ WebApplication1/WebApplication1/Controllers/ClassImageController.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Features.Classes;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClassImageController : ControllerBase
+    {
+        private readonly DBContext _context;
+        public ClassImageController(DBContext context) => _context = context;     
+        [HttpPost]
+        public async Task<IActionResult> PostClassImage(IFormFile postedFile)
+        {
+            byte[] bytes;
+
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    postedFile.OpenReadStream().CopyTo(memoryStream);
+                    bytes = memoryStream.ToArray();
+                }
+            }
+
+            string Name = Path.GetFileName(postedFile.FileName);
+            string ContentType = postedFile.ContentType;
+            byte[] Data = bytes;
+
+            ClassImage img = new ClassImage { Name = Name, ContentType = ContentType, Data = Data };
+
+            _context.ClassImages.Add(img);
+            _context.SaveChanges();
+
+            return CreatedAtAction("PostClassImage", img);
+        }
+
+
+        
+
+        /*[HttpGet]
+        public FileResult DownloadFile(int? fileId)
+        {
+            FilesEntities entities = new FilesEntities();
+            var file = entities.ClassImage.ToList().Find(p => p.id == fileId.Value);
+            return File(file.Data, file.ContentType, file.Name);
+        }
+        */
+    }
+}
