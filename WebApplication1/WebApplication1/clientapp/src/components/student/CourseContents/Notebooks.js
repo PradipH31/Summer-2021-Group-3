fb9102a823b4b0524859a174ef4bc36bfa3a00fd
pradiph31 <pradip.hamal31@gmail.com>
2021-07-07 21:08:40 -0700

    Prototype for pulling the notebooks
    Use the previous version of this file if the server does not have notebooks

--- WebApplication1/WebApplication1/clientapp/src/components/student/CourseContents/Notebooks.js
+++ WebApplication1/WebApplication1/clientapp/src/components/student/CourseContents/Notebooks.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import { red } from '@material-ui/core/colors';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { CircularProgress } from '@material-ui/core';
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
     root: {
@@ -69,14 +78,84 @@ const Notebooks = (props) => {
         );
     }
 
-    return (
-        <div>
-            <TestNoteBook />
-            <TestNoteBook />
-            <TestNoteBook />
-            <TestNoteBook />
-        </div>
-    )
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://localhost:44377/api/Notebooks")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    let itemList;
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return (
+            <div>
+                {/* <CircularProgress size={'30%'} /> */}
+                Loading
+            </div>
+        );
+    } else {
+        console.log(items)
+        itemList = items.map((item,) => {
+            return (
+                <Card className={classes.root} style={{
+                    margin: "1%"
+                }}>
+                    <CardContent>
+                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            Add in Jupyter
+                        </Typography>
+                        {/* <a href="localhost:12000/hub/user-redirect/
+                        git-pull?repo=https:%2F%2Fgithub.com%2Frevbayes%2FRevNotebooks&
+                        subPath=RB_Intro%2FRB_Intro.ipynb&app=lab"> */}
+                        <a
+                            href={`${item.githubLink}`}
+                            target="_blank"
+                        >
+                            <Typography variant="h5" component="h2">
+                                {item.title}
+                            </Typography>
+                        </a>
+                        <Typography className={classes.pos} color="textSecondary">
+                            Uploaded Date
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            {item.description}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <a
+                            href={`${item.githubLink}`}
+                            target="_blank"
+                        >
+                            <Button size="small">Open in Github</Button>
+                        </a>
+                    </CardActions>
+                </Card >
+            )
+        })
+
+        return (
+            <div>
+                {itemList}
+            </div>
+        )
+    }
 }
 
 export default Notebooks
