5f48cdcc81eeefb8ddd98dbe94e605be03ec300b
Jonathon Barber <Jonathon2997@gmail.com>
2021-07-09 09:38:42 -0500

    Starting on Enrollment

---
+++ WebApplication1/WebApplication1/Features/Courses/Enrollment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Features.Courses
+{
+    public class Enrollment
+    {
+        public int ClassId { get; set; }
+        public int UserId { get; set; }
+    }
+}
