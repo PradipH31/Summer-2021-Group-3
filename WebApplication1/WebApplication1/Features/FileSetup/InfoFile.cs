8df39bca8bcfa8d2843a9047d5af6af1fa323006
Caleb Charpentier <caleb.charpentier@selu.edu>
2021-07-11 19:32:03 -0500

    Model needed for file download data

---
+++ WebApplication1/WebApplication1/Features/FileSetup/InfoFile.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Features.FileSetup
+{
+    public class InfoFile
+    {
+        public int InfoFileId { get; set; }
+        public string Name { get; set;  }
+        public string ContentType { get; set; }
+        public string Content { get; set;  }
+     }
+}
