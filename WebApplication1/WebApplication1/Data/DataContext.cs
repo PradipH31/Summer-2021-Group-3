bcf4fd51e85a1425e6ae2be3faa0ea097d47e057
Caleb Charpentier <caleb.charpentier@selu.edu>
2021-07-12 16:02:07 -0500

    Model for flashcards

--- WebApplication1/WebApplication1/Data/DataContext.cs
+++ WebApplication1/WebApplication1/Data/DataContext.cs
@@ -9,6 +9,7 @@ using WebApplication1.Features;
 using WebApplication1.Features.Auth;
 using WebApplication1.Features.Classes;
 using WebApplication1.Features.FileSetup;
+using WebApplication1.Features.FlashCards;
 
 namespace WebApplication1.Data
 {
@@ -22,6 +23,7 @@ namespace WebApplication1.Data
         public DbSet<Course> ClassDescription { get; set; }
         public DbSet<Notebook> Notebook { get; set; }
         public DbSet<InfoFile> InfoFile { get; set; }
+        public DbSet<FlashCard> FlashCard { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
