24aabf47a93f5df776b4092e5bace91be80c6b84
Caleb Charpentier <caleb.charpentier@selu.edu>
2021-07-17 04:46:02 -0500

    FlashCardController
    FlashCards now have relations with flashcardsets

--- WebApplication1/WebApplication1/Controllers/FlashCardController.cs
+++ WebApplication1/WebApplication1/Controllers/FlashCardController.cs
@@ -30,6 +30,7 @@ namespace WebApplication1.Controllers
         public async Task<ActionResult<FlashCard>> PostFlashCard(FlashCard flashcard)
         {
             _context.FlashCard.Add(flashcard);
+
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("PostFlashCard", new { id = flashcard.FlashCardId }, flashcard);
@@ -37,9 +38,9 @@ namespace WebApplication1.Controllers
 
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Object>> GetFlashCard(int id)
+        public async Task<ActionResult<FlashCard>> GetFlashCard(int id, int flashcardsetid, int userid)
         {
-            var flashcard = await _context.FlashCard.FindAsync(id);
+            var flashcard = _context.FlashCard.Where(a => a.UserId == userid).Where(a => a.FlashCardSetId == flashcardsetid).First(b => b.FlashCardId == id);
 
             if (flashcard == null)
             {
@@ -47,19 +48,49 @@ namespace WebApplication1.Controllers
             }
 
             return flashcard;
-
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteFlashCard(int id)
+
+        //Make flashcard name and descriptions updateable
+
+        [HttpPatch("{userid}/{flashcardsetid}/{flashcardid}")]
+        public async Task<IActionResult> UpdateSetInfo(int userid, int flashcardsetid, int flashcardid, string? title = null, string? description = null)
         {
-            var flashcard = await _context.FlashCard.FindAsync(id);
+
+
+            if (description != null)
+                if (description.Length > 0)
+                {
+                    {
+                        _context.FlashCard.Where(a => a.UserId == userid).Where(b => b.FlashCardSetId == flashcardsetid).First(c => c.FlashCardId == flashcardid).Description = description;
+                    }
+                }
+
+            if (title != null)
+                if (title.Length > 0)
+                {
+                    {
+                        _context.FlashCard.Where(a => a.UserId == userid).Where(b => b.FlashCardSetId == flashcardsetid).First(c => c.FlashCardId == flashcardid).Title = title;
+                    }
+                }
+
+            await _context.SaveChangesAsync();
+
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{flashcardid}/{flashcardsetid}/{userid}")]
+        public async Task<IActionResult> DeleteFlashCard(int flashcardid, int flashcardsetid, int userid)
+        {
+            var flashcard = _context.FlashCard.Where(a => a.UserId == userid).Where(b => b.FlashCardSetId == flashcardsetid).First(c => c.FlashCardId == flashcardid);
             if (flashcard == null)
             {
                 return NotFound();
             }
 
-            _context.FlashCard.Remove(flashcard);
+            _context.FlashCardSet.First(c => c.FlashCardSetId == flashcard.FlashCardSetId).flashCard.Remove(flashcard);
             await _context.SaveChangesAsync();
 
             return NoContent();
